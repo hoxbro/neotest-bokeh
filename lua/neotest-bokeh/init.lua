8e59b4e29fccdf96a8bf8c2e8a33d8083bc86abc
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-01-10 16:02:59 +0100

    Add discover position

--- lua/neotest-bokeh/init.lua
+++ lua/neotest-bokeh/init.lua
@@ -1,15 +1,17 @@
-local neotest = {}
+local neotest_bokeh = {}
 
----@class neotest.Adapter
+---@class neotest-bokeh.Adapter
 ---@field name string
-neotest.Adapter = {}
+neotest_bokeh.Adapter = {}
 
 ---Find the project root directory given a current directory to work from.
 ---Should no root be found, the adapter can still be used in a non-project context if a test file matches.
 ---@async
 ---@param dir string @Directory to treat as cwd
 ---@return string | nil @Absolute root dir of test suite
-function neotest.Adapter.root(dir) end
+function neotest_bokeh.Adapter.root(dir)
+    return "/home/shh/projects/bokeh/bokehjs" -- TODO: Hardcoded
+end
 
 ---Filter directories when searching for test files
 ---@async
@@ -17,26 +19,53 @@ function neotest.Adapter.root(dir) end
 ---@param rel_path string Path to directory, relative to root
 ---@param root string Root directory of project
 ---@return boolean
-function neotest.Adapter.filter_dir(name, rel_path, root) end
+function neotest_bokeh.Adapter.filter_dir(name, rel_path, root)
+    return true -- TODO: Hardcoded
+end
 
 ---@async
 ---@param file_path string
 ---@return boolean
-function neotest.Adapter.is_test_file(file_path) end
+function neotest_bokeh.Adapter.is_test_file(file_path)
+    return true -- TODO: Hardcoded
+end
 
----Given a file path, parse all the tests within it.
 ---@async
 ---@param file_path string Absolute file path
 ---@return neotest.Tree | nil
-function neotest.Adapter.discover_positions(file_path) end
+function neotest_bokeh.Adapter.discover_positions(file_path)
+    local lib = require("neotest.lib")
+    local query = [[
+    ;; Match describe blocks
+    ((call_expression
+        function: (identifier) @func_name (#eq? @func_name "describe")
+        arguments: (arguments
+          (_) @namespace.name
+          (arrow_function) @namespace.definition
+        )
+    ))
+
+    ;; Match it blocks
+    ((call_expression
+        function: (identifier) @func_name (#eq? @func_name "it")
+        arguments: (arguments
+          (_) @test.name
+          (arrow_function) @test.definition
+        )
+    ))
+    ]]
+
+    ---@diagnostic disable-next-line: missing-fields
+    return lib.treesitter.parse_positions(file_path, query, { nested_namespaces = true })
+end
 
 ---@param args neotest.RunArgs
 ---@return nil | neotest.RunSpec | neotest.RunSpec[]
-function neotest.Adapter.build_spec(args) end
+function neotest_bokeh.Adapter.build_spec(args) end
 
 ---@async
 ---@param spec neotest.RunSpec
 ---@param result neotest.StrategyResult
 ---@param tree neotest.Tree
 ---@return table<string, neotest.Result>
-function neotest.Adapter.results(spec, result, tree) end
+function neotest_bokeh.Adapter.results(spec, result, tree) end
