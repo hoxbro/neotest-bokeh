13bd3c509711b9f44c607366bc10b872c3bef53b
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-01-13 17:00:53 +0100

    test: Add more tests

--- tests/init_spec.lua
+++ tests/init_spec.lua
@@ -1,5 +1,6 @@
 local async = require("nio.tests")
 local neotest_bokehjs = require("neotest-bokehjs")
+
 -- local nio = require("nio")
 
 -- nio.run(function()
@@ -11,6 +12,41 @@ local neotest_bokehjs = require("neotest-bokehjs")
 --         file:close()
 --     end
 -- end)
+
+local root_dir = vim.uv.cwd() .. "/tests/data/bokehjs/"
+
+describe("root_dir", function()
+    async.it("check if root_dir is not set", function()
+        assert.are_nil(neotest_bokehjs.root_dir)
+        assert.are_nil(neotest_bokehjs.root("not relevant, is set with __call"))
+    end)
+    async.it("check if root_dir is set", function()
+        neotest_bokehjs({ root_dir = root_dir })
+        assert.are_equal(root_dir, neotest_bokehjs.root_dir)
+        assert.are_equal(root_dir, neotest_bokehjs.root("not relevant, set with __call"))
+    end)
+end)
+
+describe("filter_dir", function()
+    local test_fn = function(x) return neotest_bokehjs.filter_dir("", x, "") end
+    async.it("good 1", function() assert.are_true(test_fn("test")) end)
+    async.it("good 2", function() assert.are_true(test_fn("test/unit/")) end)
+    async.it("bad 1", function() assert.are_false(test_fn("tests")) end)
+    async.it("bad 2", function() assert.are_false(test_fn("tests/unit/")) end)
+    async.it("bad 3", function() assert.are_false(test_fn("example/test/")) end)
+    async.it("bad 4", function() assert.are_false(test_fn("example/test/unit/")) end)
+    async.it("bad 5", function() assert.are_false(test_fn("")) end)
+end)
+
+describe("is_test_file", function()
+    local test_fn = function(x) return neotest_bokehjs.is_test_file(root_dir .. x) end
+    async.it("good 1", function() assert.are_true(test_fn("/test/unit/index.ts")) end)
+    async.it("good 2", function() assert.are_true(test_fn("/test/unit/sub/index.ts")) end)
+    async.it("bad 1", function() assert.are_false(test_fn("/test/unit/index.d.ts")) end)
+    async.it("bad 2", function() assert.are_false(test_fn("/test/unit/_index.ts")) end)
+    async.it("bad 3", function() assert.are_false(test_fn("/test/unit/_index.ts")) end)
+    async.it("bad 4", function() assert.are_false(test_fn("/test/unit/index.js")) end)
+end)
 
 describe("discover_positions", function()
     async.it("discover_positions in test_file.ts", function()
